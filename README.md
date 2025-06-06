28250c0de3a14842a3e690aede2611f0e50e6dde
Kirill <kirilldevelop1511@mail.ru>
2025-06-06 20:40:43 +0300

    Main functional

---
+++ README.md
@@ -0,0 +1,17 @@
+# What is this?
+It's simple **Todo List**
+You can add tasks, mark them as compleate are tasks and delete theme.
+
+## Example of uses
+- Main menu
+<div> 
+    <img src="Resource/Example.png" \>
+</div>
+
+- Modal for adding task
+<div>
+    <img src="Resource/ModalExample.png" \>
+</div>
+
+# Future
+Add saving to localStore. I am want add adaptive for phone and other more smaller devices. I will also do a delete for the task that is completed
---
+++ index.js
@@ -0,0 +1,97 @@
+const modalBtns = document.querySelector("*[data-modal-btn]");
+let name = modalBtns.getAttribute('data-modal-btn');
+let modal = document.querySelector("*[data-modal-window='"+name+"']");
+const addBtn = modal.querySelector(".add_button");
+const closeBtn = modal.querySelector(".cancel_button");
+
+let tasks = []
+updateTasks()
+
+function completeTask(index) {
+    tasks[index].isCompleted = tasks[index].isCompleted ? false : true;
+    updateTasks()
+}
+
+function deleteTask(index) {
+    tasks = tasks.filter((task) => task.id !== tasks[index].id);
+    updateTasks();
+}
+
+function updateTasks() {
+    let taskCard = document.querySelector(".task_cards");
+    taskCard.innerHTML = '';
+    if (tasks.length == 0) {
+        const noTasks = `
+            <div class="no_tasks">
+                <h1>Пока что нету задач</h1>
+            </div>
+        `
+        taskCard.insertAdjacentHTML('beforeend', noTasks);
+    } 
+    tasks.forEach((element, index, array) =>  {
+        const task = `
+        <li class="card">
+            
+            <h3>${element.title}</h3>
+            <div class="button_section">
+                <button class="compleate_button ${index}"><img src="Resource/check-3277.svg"></button>
+                <button class="delete_button"><img src="Resource/trash-can-10413.svg" alt=""></button>
+            </div>
+        </li>
+        `;
+        
+        taskCard.insertAdjacentHTML('beforeend', task);
+        if (element.isCompleted) {
+            taskCard.querySelectorAll("h3")[index].style.textDecoration = "line-through";
+        }   
+    })
+    const compleatedBtns = document.querySelectorAll(".compleate_button");
+    for (let i = 0; i < compleatedBtns.length; i++) {
+        compleatedBtns[i].removeEventListener('click', () => {
+            completeTask(i);
+        })
+        compleatedBtns[i].addEventListener('click', () => {
+            completeTask(i);
+        })
+    }
+    const deleteBtns = document.querySelectorAll(".delete_button");
+    for (let i = 0; i < deleteBtns.length; i++) {
+        deleteBtns[i].removeEventListener('click', () => {
+            deleteTask(i);
+        })
+        deleteBtns[i].addEventListener('click', () => {
+            deleteTask(i);
+        })
+    }
+}
+
+function addElement(title, description, index) {
+    let id = tasks.at(-1) ? tasks.at(-1).id + 1 : 0;
+    let task = {
+        id: id,
+        title: title,
+        descriptio: description,
+        isCompleted: false
+    }
+    tasks.push(task);
+}
+
+modalBtns.addEventListener('click', () => {
+    modal.style.display = "block";
+})
+
+
+addBtn.addEventListener('click', () => {
+    let inputs = modal.querySelectorAll('input');
+    let title = inputs[0].value;
+    let description = inputs[1].value;
+    addElement(title, description);
+    inputs[0].value = "";
+    inputs[1].value = "";
+    updateTasks();
+    modal.style.display = "none";
+})
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = "none";
+})
